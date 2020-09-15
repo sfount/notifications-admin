c7737a848781baad74e3c5522e1af462ae7c47f4
Tom Byers <tombaromba@gmail.com>
2020-09-15 22:09:03 +0100

    Update autofocused elements label on page load
    Also removes it once focus has moved.

--- app/assets/javascripts/autofocus.js
+++ app/assets/javascripts/autofocus.js
@@ -3,13 +3,22 @@
 
   Modules.Autofocus = function() {
     this.start = function(component) {
-      var forceFocus = $(component).data('forceFocus');
+      var $component = $(component),
+          forceFocus = $component.data('forceFocus'),
+          labelText = $('label[for="' + $component.attr('id') + '"]').eq(0).text().trim(),
+          clearAriaLabel = evt => {
+            $component.removeAttr('aria-label');
+            $component.off('blur', clearAriaLabel);
+          };
 
       // if the page loads with a scroll position, we can't assume the item to focus onload
       // is still where users intend to start
       if (($(window).scrollTop() > 0) && !forceFocus) { return; }
 
-      $(component).filter('input, textarea, select').eq(0).trigger('focus');
+      $component.attr('aria-label', document.title + ' - ' + labelText);
+
+      $component.filter('input, textarea, select').eq(0).trigger('focus');
+      $component.on('blur', clearAriaLabel);
 
     };
   };
